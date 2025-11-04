9177a684b9bf66f9e62e10fa532028f63e1b7064
MarkJFI <figueroamark67@gmail.com>
2025-11-04 09:35:56 -0500

    04/11--2

--- view/login.php
+++ view/login.php
@@ -6,7 +6,7 @@
   <title>login</title>
   <style>
     body {
-      background-image: url('https://img.freepik.com/fotos-premium/manos-mujer-hermosa-telefono-tarjeta-credito-mujer-joven-bolsas-compras_217236-7963.jpg');
+      background-image: url('https://i.pinimg.com/736x/55/74/54/557454f9d762c6b6c9efdaec66b4dbb9.jpg');
       background-repeat: no-repeat;
       background-size: cover;
     }
@@ -17,10 +17,10 @@
 
     .login {
 
-      width: 250px;
-      margin: 100px auto;
-      padding: 20px;
-      border: 2px solid #e7dcdcff;
+      width: 350px;
+      margin: 250px auto;
+      padding: 50px;
+      border: 1px solid #131212ff;
       border-radius: 100px;
       text-align: center;
       font-family: sans-serif;
@@ -29,18 +29,18 @@
 
     input {
       display: block;
-      width: 90%;
-      margin: 10px auto;
-      padding: 8px;
+      width: 100%;
+      margin: 20px auto;
+      padding: 10px;
       border-radius: 20px;
 
 
     }
 
     button {
-      background: linear-gradient(rgba(230, 175, 23, 1), rgb(238, 252, 252));
-      width: 150px;
-      height: 35px;
+      background: #78f503cb;
+      width: 160px;
+      height: 40px;
       border-radius: 20px;
     }
   </style>
@@ -55,7 +55,7 @@
     <form id="frm_login">
       <center> <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjopiHq_3AR78yIAK34Wd5y4dUbvEBUHTs-DbnZnaqA8v8-nyFhVMCeLSVm0KF4VrFrKZrwl4pdXfiHhaYjaj1Spc3XbhuhCzJS8-OOLfb7m4kvuUr_fK15GSNDcobHeUkRNv8NuCpGkfyA/s900/Las-Grandes-Empresas-no-Despengan-en-La-Venta-Online.gif" alt="" width="250px" height="200px" </center>
         <input type="text" placeholder="Usuario" id="User" name="username">
-        <input type="tex" placeholder="Contraseña" id="Contraseña" name="password">
+        <input type="password" placeholder="Contraseña" id="Contraseña" name="password">
         <button type="button" placeholder="Ingresar" onclick="iniciar_sesion();">Ingresar</button>
     </form>
     <br>
